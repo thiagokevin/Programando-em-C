4436af241b5e89765b327e0b2da0cbe9e93dbe88
Kevinn.h0 <91503622+thiagokevin@users.noreply.github.com>
2023-02-28 23:02:30 +0000

    nota do aluno

---
+++ NotaDoAluno.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+    float notas[10],media = 0;
+    int i;
+    for(i=0;i<10;i++)
+    {
+        printf("\nInforme a nota do Aluno %d:-> ",(i+1));
+        scanf("%f", &notas[i]);
+        media += notas[i];
+
+    }
+    media /= 10;
+    printf("\nNotas dos Alunos\n");
+    for(i=o;i<10;i++)
+    {
+        printf("\nAlunos %2d - Nota = %5.2f",(i+1));
+        printf("\nMedia total dos alunos:-> %.2f",media);
+    }
+}
